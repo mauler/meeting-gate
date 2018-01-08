f10a38ff41eeb2651d8f7d7cf6c233682f1db0ae
Paulo R <proberto.macedo@gmail.com>
2018-01-08 02:42:11 -0200

    Added missing dependency

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     install_requires=[
         'django',
         'djangorestframework',
+        'meeting',
     ],
     zip_safe=False,
     keywords='festival rfid qrcode barcode tickets',
