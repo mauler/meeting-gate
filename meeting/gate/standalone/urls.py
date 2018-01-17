a7f19a28dabb4f9414ae5d161ef2283a9cf79e12
Paulo R <proberto.macedo@gmail.com>
2018-01-17 07:20:57 -0200

    Changed admin url

--- meeting/gate/standalone/urls.py
+++ meeting/gate/standalone/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.conf import settings
+from django.contrib import admin
 
 
 urlpatterns = [] + \
     static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT) + \
     [
+        url(r'^Meeting-Gate-Admin/', admin.site.urls),
         url(r'', include('meeting.gate.urls', namespace='meeting-gate')),
     ]
